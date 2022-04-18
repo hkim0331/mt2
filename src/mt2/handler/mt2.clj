04e182b8337567e97bce33ede0e03a1e3582a906
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-04-18 20:08:26 +0900

    title 'micro twitter'

--- src/mt2/handler/mt2.clj
+++ src/mt2/handler/mt2.clj
@@ -57,7 +57,7 @@
      [:meta {:charset "utf-8"}]
      [:meta {:name "viewport"
              :content "width=device-width, initial-scale=1, shrink-to-fit=no"}]
-     [:title "private micro twitter"]
+     [:title "micro twitter"]
      (hiccup/include-css
       "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css")]
     [:body
